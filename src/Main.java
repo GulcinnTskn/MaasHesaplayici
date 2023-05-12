27f4c962df14c3e7037900b546c3d225a413a831
Gülçin <gulcintskn18@gmail.com>
2023-05-12 18:49:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+      Employee employee1 = new Employee("Gülçin",15000,45,2017);
+      Employee employee2 = new Employee("Fatih",12000,40,2006);
+
+      employee1.printInfo();
+      employee2.printInfo();
+    }
+}
